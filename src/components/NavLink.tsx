406e06a64664f9f172a89f376c2c9f9e34c2bf69
zakhoudache <cssftj2@gmail.com>
2026-03-24 07:11:11 +0100

    bb

--- src/components/NavLink.tsx
+++ src/components/NavLink.tsx
@@ -1,28 +1,44 @@
-import { NavLink as RouterNavLink, NavLinkProps } from "react-router-dom";
-import { forwardRef } from "react";
+import * as React from "react";
+import {
+  NavLink as RouterNavLink,
+  NavLinkProps,
+} from "react-router-dom";
+
 import { cn } from "@/lib/utils";
 
-interface NavLinkCompatProps extends Omit<NavLinkProps, "className"> {
+type ExtraProps = {
   className?: string;
   activeClassName?: string;
   pendingClassName?: string;
-}
+};
 
+type NavLinkCompatProps = Omit<NavLinkProps, "className"> & ExtraProps;
 
-const NavLink = forwardRef<HTMLAnchorElement, NavLinkCompatProps>(
-  ({ className, activeClassName, pendingClassName, to, ...props }, ref) => {
-    return (
-      <RouterNavLink
-        ref={ref}
-        to={to}
-        className={({ isActive, isPending }) =>
-          cn(className, isActive && activeClassName, isPending && pendingClassName)
-        }
-        {...props}
-      />
-    );
-  },
-);
+const NavLink = React.forwardRef<
+  HTMLAnchorElement,
+  NavLinkCompatProps
+>((props, ref) => {
+  const {
+    className,
+    activeClassName,
+    pendingClassName,
+    ...rest
+  } = props;
+
+  return (
+    <RouterNavLink
+      ref={ref}
+      {...rest}
+      className={({ isActive, isPending }) =>
+        cn(
+          className,
+          isActive && activeClassName,
+          isPending && pendingClassName
+        )
+      }
+    />
+  );
+});
 
 NavLink.displayName = "NavLink";
 
